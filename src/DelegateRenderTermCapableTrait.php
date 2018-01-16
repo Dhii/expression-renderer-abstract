d348cc03527dc5cf6106e4dbcb2ea3629edb02db
Miguel Muscat <miguelmuscat93@gmail.com>
2018-01-16 13:19:20 +0100

    `DelegateRenderTermCapableTrait` catches ...
    ... `OutOfRangeException` and wraps it in a `RendererException`.

--- src/DelegateRenderTermCapableTrait.php
+++ src/DelegateRenderTermCapableTrait.php
@@ -7,6 +7,7 @@ use Dhii\Output\Exception\RendererExceptionInterface;
 use Dhii\Output\Exception\TemplateRenderExceptionInterface;
 use Dhii\Output\TemplateInterface;
 use Dhii\Util\String\StringableInterface as Stringable;
+use Exception as RootException;
 use OutOfRangeException;
 
 /**
@@ -30,7 +31,15 @@ trait DelegateRenderTermCapableTrait
      */
     protected function _delegateRenderTerm(TermInterface $term)
     {
-        return $this->_getTermDelegateRenderer($term)->render($term);
+        try {
+            return $this->_getTermDelegateRenderer($term)->render($term);
+        } catch (OutOfRangeException $outOfRangeException) {
+            throw $this->_createRendererException(
+                $this->__('Could not find a delegate renderer for the given term.'),
+                null,
+                $outOfRangeException
+            );
+        }
     }
 
     /**
@@ -45,4 +54,35 @@ trait DelegateRenderTermCapableTrait
      * @throws OutOfRangeException If no renderer can be retrieved for the given term type.
      */
     abstract protected function _getTermDelegateRenderer(TermInterface $term);
+
+    /**
+     * Creates a new invalid argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     *
+     * @return RendererExceptionInterface The new exception.
+     */
+    abstract protected function _createRendererException(
+        $message = null,
+        $code = null,
+        RootException $previous = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see   sprintf()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
 }
